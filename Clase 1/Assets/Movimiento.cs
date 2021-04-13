96bb944c01193db219b59d47b02527eca8571a04
ptrcia <patryechelon@gmail.com>
2021-04-13 20:55:35 +0200

    Subir Proyecto Unity Prueba
    Comprobamos si nos funciona a todas hacer un pull del trabajo y visualizarlo bien.

---
+++ Clase 1/Assets/Movimiento.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    public float speed; // publico, esto se puede modificar
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal")*speed*Time.deltaTime, 0.0f, 0.0f)); //movimiento derecha izquierdamultiplicado por la velocidad
+        
+    }
+}
