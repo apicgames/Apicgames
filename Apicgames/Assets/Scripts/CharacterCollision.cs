207789ccdf21e79a28f3f66dcdb578cda317cdad
Alicia Martinez <alicialicia9816@gmail.com>
2021-04-16 22:49:00 +0200

    Subida proyecto MUY básico
    Se elimina el proyecto que había y se crea uno nuevo (versión 2019.4.23f) que contiene el funcionamiento MUY básico de la escena principal

---
+++ Apicgames/Assets/Scripts/CharacterCollision.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCollision : MonoBehaviour
+{
+    //UI con los minijuegos
+    public GameObject[] minijuego;
+    Animator anim;
+
+    void Start()
+    {
+
+        //Escondemos todos los minijuegos
+        for (var i = 0; i < minijuego.Length; i++)
+        {
+            minijuego[i].SetActive(false);
+        }
+
+    }
+
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Minijuego 1 > Señora mayor
+        if (other.gameObject.name == "1D")
+        {
+            minijuego[0].SetActive(true);
+        }
+        //Minijuego 2 > Fachamilia
+        else if (other.gameObject.name == "3I")
+        {
+            minijuego[1].SetActive(true);
+        }
+        //Minijuego 3 > Destender ropa
+        else if (other.gameObject.name == "4D")
+        {
+            minijuego[2].SetActive(true);
+        }
+    }
+}
---
+++ Apicgames/Assets/Scripts/MinijuegoUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MinijuegoUI : MonoBehaviour
+{
+    public Button closeButton;
+    public GameObject UIinterface;
+
+    void Start()
+    {
+        Button btn = closeButton.GetComponent<Button>();
+        btn.onClick.AddListener(closeMinigame);
+    }
+
+    void closeMinigame()
+    {
+        UIinterface.SetActive(false);
+    }
+}
